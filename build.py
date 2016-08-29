cb0cd28ae61e45b0cf539d37b063d903367faa8e
New Future <new_future@msn.cn>
2016-08-30 00:12:01 +0800

    auto build

--- build.py
+++ build.py
@@ -3,10 +3,9 @@
 #encoding=utf-8
 #coding:utf-8
 import time
+import sys
 
-#输出文件
-output = open('nkulwan.py','w')
-
+#header
 header = '''\
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
@@ -24,7 +23,7 @@ if __name__ == "__main__":
         loop()
 '''
 
-def INCLUDE(filename,start_tag="import",end_tag="if __name__ == '__main__'"):
+def INCLUDE(output,filename,start_tag="import",end_tag="if __name__ == '__main__'"):
     with open(filename,'r') as gateway:
         print 'include:',filename
         for line in gateway:
@@ -38,10 +37,13 @@ def INCLUDE(filename,start_tag="import",end_tag="if __name__ == '__main__'"):
                 output.write(line)
 
 if __name__ == '__main__':
-
+    output_file=  len(sys.argv) > 1 and sys.argv[1] or 'nkulwan.py'
+    print "building to: [%s] ..."%output_file
+    output= open(output_file,'w')
     output.write(header.format(buildtime=time.ctime()))
-    INCLUDE("gateway.py")
-    INCLUDE("login.py",'#START_TAG')
-    INCLUDE("logout.py",'#START_TAG')
+    INCLUDE(output,"gateway.py")
+    INCLUDE(output,"login.py",'#START_TAG')
+    INCLUDE(output,"logout.py",'#START_TAG')
     output.write(main)
     output.close()
+    print "Done!"
---
+++ login.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from gateway import *
+#START_TAG
+import sys
+import socket
+
+# 配置
+account  = None #"网关账号[学号]"
+password = None #网关登录密码
+cir_time = 60   #循环时间(s)
+TIMEOUT  = 10   #连接超时时间(s)
+
+socket.setdefaulttimeout(TIMEOUT)
+
+
+def getAccount(): #获取账号
+    import getpass
+    global account,password 
+    account = account or raw_input("input username [ 学号或账号 ]:")
+    password = password or getpass.getpass("input password [ 校园网密码 ]:")
+    return login(account,password)
+        
+def auto():  #自动登陆
+    global account,password
+
+    socket.setdefaulttimeout(4)
+    result = query()
+    socket.setdefaulttimeout(TIMEOUT)
+    
+    if result and result['uid']:
+        print 'ONLine: ', result
+        return True
+    else:
+        print 'OFFLine, try login!'
+        getAccount()
+        result = login(account, password)
+        if result:
+            print 'Login SUCCESS:', result
+            return True
+        else:
+            return False
+
+def loop(): #循环登录
+    import time
+    global cir_time,TIMEOUT,account,password
+    
+    socket.setdefaulttimeout(2)
+    logout()
+    socket.setdefaulttimeout(3)
+    while not getAccount():
+        password=None
+        print "%s try login fialed!"%account
+        print NET_ERROR
+    else:
+        print "Login SUCCESS! [登录%s成功]"%account
+    
+    socket.setdefaulttimeout(TIMEOUT)
+    while True:
+        print time.ctime()
+        if auto():
+            time.sleep(cir_time) #每隔cir_time秒执行一次
+        else:
+            time.sleep(cir_time/5) #每隔cir_time秒执行一次
+
+
+if __name__ == '__main__':
+    print "Login NKUWlan"
+    if len(sys.argv) > 1 : account = sys.argv[1]
+    if len(sys.argv) > 2 : password = sys.argv[2]
+    print "waiting..."
+    loop()
