a8a06c565a1919fdb1fd0178f4b911673a49d4ca
From-Travis-CI <github@newfuture.cc>
2016-10-24 04:10:33 +0000

    :eight_spoked_asterisk: Auto Build

--- nkuwlan.py
+++ nkuwlan.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__version__ = '2.0.0'
+__version__ = '2.0.2'
 __author__ = 'New Future'
 
-# THIS FILE AUTO BUILD AT--- Sun Oct 23 14:57:36 2016 ---
+# THIS FILE AUTO BUILD AT--- Mon Oct 24 04:10:32 2016 ---
 
 #include form file [nkuwlan/gateway.py] 
 import urllib
