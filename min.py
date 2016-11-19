343c195af5fa677e338e28eaffebf6f1c0892a1c
From-Travis-CI <github@newfuture.cc>
2016-11-19 07:45:26 +0000

    :angel: Auto Build

--- min.py
+++ min.py
@@ -1,86 +1,112 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-__version__='2.0.0';__author__='New Future';import base64 as R;import json as L;import os as e;import sys as u;import time as y;from distutils.version import StrictVersion as S;from hashlib import sha512 as V;from uuid import getnode as ac;_a=e.path.expanduser('~');B=[_a+'/.nkuwlan/conf.json',_a+'/.nkuwlan.json','/etc/nkuwlan/conf.json']
-def C(a=None):
- a=w(a)
- if a:
-  try:
-   o=D(a)
-   with open(a,'r')as K:
-    b=L.load(K);b['password']=M(b,o)
-    if not b['password']:return False
-   e.utime(a,(o['AT'],o['MT']));return b
-  except Exception,x:print'load config failed: %s'%x;return False
-def N(b,a=None):
- a=w(a)or B[0];dir=e.path.dirname(a)
- try:
-  if not e.path.exists(dir):e.mkdir(dir,448)
-  if not e.path.isfile(a):
-   if e.name=='nt':open(a,'w').close()
-   else:e.mknod(a,384)
-  b['version']=__version__;b['password'],s,t=O(b,a)
-  with open(a,'w')as P:P.write(L.dumps(b))
-  e.utime(a,(s,t));return a
- except Exception,x:print'save error: %s'%x;return False
-def ab(a=None):
- a=w(a)
- if a:e.remove(a);return a
-def w(a=None):
- if a:return a
- for a in B:
-  if e.path.isfile(a):return a
-def O(b,path):
- d=D(path);s,t=round(y.time())+10,round(d['CT']);d['AT'],d['MT']=float(s),float(t);i,k,l=E(d,b['username']);F=k+b['password']+l;c=[]
- for q in range(len(F)):Q=chr((ord(F[q])+ord(i[q%len(i)]))%256);c.append(Q)
- c=''.join(c)
- if u.version_info[0]==3:c=c.encode()
- c=R.urlsafe_b64encode(c).decode();return[c,s,t]
-def M(b,o):
- if not'version'in b or S(b['version'])<S('1.0.0'):return b['password']
- i,k,l=E(o,b['username']);c=b['password'].encode();c=R.urlsafe_b64decode(c)
- if u.version_info[0]==3:c=c.decode()
- n=[]
- for q in range(len(c)):T=chr((ord(c[q])-ord(i[q%len(i)]))%256);n.append(T)
- n=''.join(n)
- if n.startswith(k)and n.endswith(l):return n[len(k):-len(l)]
- else:print'\nconfig file verification failed!';return False
-def D(a):f=e.stat(a);return{'P':a,'mac':ac(),'M':f.st_mode,'N':f.st_ino,'D':f.st_dev,'L':f.st_nlink,'U':f.st_uid,'G':f.st_gid,'CT':f.st_ctime,'AT':f.st_atime,'MT':f.st_mtime}
-def E(d,U):d['CT']=0;d=V(repr(sorted(d.items())).encode('utf-8')).hexdigest();i=V((U+d).encode('utf-8')).hexdigest();G=str(int(d,16));k,l=d[:int(G[3])],d[-int(G[-1]):];return[i,k,l]
-from socket import setdefaulttimeout as r;j=None;h=None;H=60;z=10;r(z)
-def A(W=True):
- global j,h;import getpass as ad;b=W and C()
- if b:j=b['username'];h=b['password']
- else:print u.argv[0],'-s to save';j=raw_input('input username:');h=ad.getpass('input password:')
- return X(j,h)
-def I():
- r(4);g=ae();r(z)
- if g and g['uid']:print'ONLine: ',g;return True
- else:
-  print'OFFLine, try login!';A();g=X(j,h)
-  if g:print'Login SUCCESS:',g;return True
-  else:return False
-def Y():
- global h
- if not C():r(2);Z()
- r(3)
- while not A():h=None;print'%s try login fialed!\n%s'%(j,error())
- else:print'Login SUCCESS!'
- r(z)
- while True:
-  print y.ctime()
-  if I():y.sleep(H)
-  else:y.sleep(H/5)
+__version__='2.1.0';__author__='New Future'
+try:import urllib2 as C;from urllib import urlencode as az
+except ImportError:import urllib.request as C;from urllib.parse import urlencode as az
+D='http://202.113.18.106',;ay='/';ac=':801/eportal/?c=ACSetting&a=Login';ad=':801/eportal/?c=ACSetting&a=Logout';M=None
+def N(*E):
+	if len(E)<=0:E=D
+	for q in E:
+		t=F(q)
+		if t:
+			O=find(t,"flow='");P=find(t,"fee='");d={'fee':P and int(P),'flow':O and int(O)/1024,'uid':find(t,"uid='"),'ipv4':find(t,"v4ip='")}
+			if d['uid']:return d
+def Q(R,x,ae=0):
+	k={'upass':x}
+	if ae:k.update(DDDDD='_'+R,login_nei=1)
+	else:k['DDDDD']=R
+	for q in D:
+		G=q+ac;F(G,k);d=N(q)
+		if d and d['uid']:return d
+def S():
+	for q in D:F(q+ad)
+def error():return M
+def find(u,h,af="'"):
+	T=u.find(h)
+	if T>0:u=u[len(h)+T:];return u[:u.find(af)].strip()
+def F(G,k=None,ag='gb2312'):
+	try:k=k and az(k).encode();C.getproxies=lambda:{};return C.urlopen(G,k).read().decode(ag)
+	except Exception,ah:M=ah;return None
+import base64 as ap;import json as aj;import os as g;import sys as A;import time as J;from distutils.version import StrictVersion as aq;from hashlib import sha512 as au;from uuid import getnode as aB;_a=g.path.expanduser('~');U=[_a+'/.nkuwlan/conf.json',_a+'/.nkuwlan.json','/etc/nkuwlan/conf.json']
+def V(a=None):
+	a=H(a)
+	if a:
+		try:
+			v=W(a)
+			with open(a,'r')as ai:
+				b=aj.load(ai);b['password']=ak(b,v)
+				if not b['password']:return False
+			g.utime(a,(v['AT'],v['MT']));return b
+		except Exception,I:print'load config failed: %s'%I;return False
+def al(b,a=None):
+	a=H(a)or U[0];dir=g.path.dirname(a)
+	try:
+		if not g.path.exists(dir):g.mkdir(dir,448)
+		if not g.path.isfile(a):
+			if g.name=='nt':open(a,'w').close()
+			else:g.mknod(a,384)
+		b['version']=__version__;b['password'],y,z=am(b,a)
+		with open(a,'w')as an:an.write(aj.dumps(b))
+		g.utime(a,(y,z));return a
+	except Exception,I:print'save error: %s'%I;return False
+def aA(a=None):
+	a=H(a)
+	if a:g.remove(a);return a
+def H(a=None):
+	if a:return a
+	for a in U:
+		if g.path.isfile(a):return a
+def am(b,path):
+	f=W(path);y,z=round(J.time())+10,round(f['CT']);f['AT'],f['MT']=float(y),float(z);n,h,r=X(f,b['username']);x=h+b['password']+r;e=[]
+	for i in range(len(x)):ao=chr((ord(x[i])+ord(n[i%len(n)]))%256);e.append(ao)
+	e=''.join(e)
+	if A.version_info[0]==3:e=e.encode()
+	e=ap.urlsafe_b64encode(e).decode();return[e,y,z]
+def ak(b,v):
+	if not'version'in b or aq(b['version'])<aq('1.0.0'):return b['password']
+	n,h,r=X(v,b['username']);e=b['password'].encode();e=ap.urlsafe_b64decode(e)
+	if A.version_info[0]==3:e=e.decode()
+	s=[]
+	for i in range(len(e)):ar=chr((ord(e[i])-ord(n[i%len(n)]))%256);s.append(ar)
+	s=''.join(s)
+	if s.startswith(h)and s.endswith(r):return s[len(h):-len(r)]
+	else:print'\nconfig file verification failed!';return False
+def W(a):j=g.stat(a);return{'P':a,'mac':aB(),'M':j.st_mode,'N':j.st_ino,'D':j.st_dev,'L':j.st_nlink,'U':j.st_uid,'G':j.st_gid,'CT':j.st_ctime,'AT':j.st_atime,'MT':j.st_mtime}
+def X(f,at):f['CT']=0;f=au(repr(sorted(f.items())).encode('utf-8')).hexdigest();n=au((at+f).encode('utf-8')).hexdigest();Y=str(int(f,16));h,r=f[:int(Y[3])],f[-int(Y[-1]):];return[n,h,r]
+from socket import setdefaulttimeout as w;o=None;l=None;Z=60;K=10;w(K)
+def L(av=True):
+	global o,l;import getpass as aC;b=av and V()
+	if b:o=b['username'];l=b['password']
+	else:print A.argv[0],'-s to save';o=raw_input('input username:');l=aC.getpass('input password:')
+	return Q(o,l)
 def aa():
- b={'username':j,'password':h};g=N(b)
- if g:print'saved to',g;return True
- else:print'save failed!'
-def J():print'logout...';Z();print'Done!'
+	w(4);d=N();w(K)
+	if d and d['uid']:print'ONLine: ',d;return True
+	else:
+		print'OFFLine, try login!';L();d=Q(o,l)
+		if d:print'Login SUCCESS:',d;return True
+		else:return False
+def aw():
+	global l
+	if not V():w(2);S()
+	w(3)
+	while not L():l=None;print'%s try login fialed!\n%s'%(o,error())
+	else:print'Login SUCCESS!'
+	w(K)
+	while True:
+		print J.ctime()
+		if aa():J.sleep(Z)
+		else:J.sleep(Z/5)
+def ax():
+	b={'username':o,'password':l};d=al(b)
+	if d:print'saved to',d;return True
+	else:print'save failed!'
+def ab():print'logout...';S();print'Done!'
 if __name__=='__main__':
- v=u.argv[1:]and u.argv[1].lower()
- if v=='logout':J()
- elif v=='loop':Y()
- elif v=='-s':
-  J()
-  if A(False):aa()
- elif v=='-v':print __version__
- else:I()
+	B=A.argv[1:]and A.argv[1].lower()
+	if B=='logout':ab()
+	elif B=='loop':aw()
+	elif B=='-s':
+		ab()
+		if L(False):ax()
+	elif B=='-v':print __version__
+	else:aa()
--- nkuwlan.py
+++ nkuwlan.py
@@ -1,9 +1,141 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 __author__ = 'New Future'
 
-# THIS FILE AUTO BUILD AT--- Sat Nov 19 06:56:30 2016 ---
+# THIS FILE AUTO BUILD AT--- Sat Nov 19 07:45:25 2016 ---
+
+#include form file [nkuwlan/gateway.py] 
+try:
+    # from urllib2 as urlopen
+    import urllib2 as req
+    from urllib import urlencode
+except ImportError:
+    # python 3
+    import urllib.request as req
+    from urllib.parse import urlencode
+
+# 网关 地址
+HOST = ('http://202.113.18.106',)
+# url路径
+QUERY_PATH = '/'
+LOGIN_PATH = ':801/eportal/?c=ACSetting&a=Login'
+LOGOUT_PATH = ':801/eportal/?c=ACSetting&a=Logout'
+# 错误信息
+NET_ERROR = None
+
+
+def query(*qhost):  # 查询
+    """query info from NKU_WLAN.
+
+    查询账号信息和登录状态
+
+    Args:
+        qhost: 查询主机，如果没有指定，会尝试主机列表所有主机 .
+
+    Returns:
+        查询成功返回字典，显示账号当前信息；
+        查询失败None
+
+    Raises:
+
+    """
+    # 主机列表
+    if len(qhost) <= 0:
+        # 指定主机使用默认
+        qhost = HOST
+    # 逐个查询直到命中
+    for host in qhost:
+        html = request(host)
+        if html:  # 网关异常直接换其他网关
+            flow = find(html, "flow='")
+            fee = find(html, "fee='")
+            result = {
+                'fee': fee and int(fee),
+                'flow': flow and int(flow) / 1024,
+                'uid': find(html, "uid='"),
+                'ipv4': find(html, "v4ip='")
+            }
+            if result['uid']:  # 查询到登录ID返回
+                return result
+
+
+def login(user, pwd, internal=0):  # 登录
+    """login NKU_WLAN.
+
+    登录网关
+
+    Args:
+        user: 登录账号 login account .
+        pwd: 登录密码 password.
+        internal: 是否只登录内网
+
+    Returns:
+        登录成功返回字典，显示账号当前信息；
+        登录失败None
+
+    Raises:
+
+    """
+    data = {'upass': pwd}
+    if internal:
+        data.update(DDDDD='_' + user, login_nei=1)
+    else:
+        data['DDDDD'] = user
+    for host in HOST:
+        url = host + LOGIN_PATH
+        request(url, data)
+        result = query(host)
+        if result and result['uid']:
+            return result
+
+
+def logout():  # 注销
+    """logout NKU_WLAN.
+
+    注销网关尝试host列表的所有网关记录，一一注销
+    """
+    for host in HOST:
+        request(host + LOGOUT_PATH)
+
+
+def error():  # 获取错误信息
+    """get last error info.
+
+    获取上次出错信息
+
+    Returns:
+        Exception NET_ERROR
+        未发生错误返回 None
+    """
+    return NET_ERROR
+
+
+def find(content, start, endtag="'"):  # 查找
+    """find string in html
+        查找文档中start 和 end 之间的的内容
+    Returns:
+       string
+    """
+    pos = content.find(start)
+    if pos > 0:
+        content = content[len(start) + pos:]
+        return content[:content.find(endtag)].strip()
+
+
+def request(url, data=None, code='gb2312'):  # 获取网页
+    """ resquet a url
+        发送请求
+    """
+    try:
+        data = data and urlencode(data).encode()
+        req.getproxies = lambda: {}
+        return req.urlopen(url, data).read().decode(code)
+    except Exception as err:
+        NET_ERROR = err
+        return None
+
+
 
 #include form file [nkuwlan/config.py] 
 import base64
