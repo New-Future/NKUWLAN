02d0503f1948068f0687de3fd8167e9380347c5b
NewFuture <github@newfuture.cc>
2016-11-19 15:44:54 +0800

    update build and version

--- build.py
+++ build.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# encoding=utf-8
 # coding:utf-8
 import time
 import sys
 
 
-main = '''
+MAIN = '''
 if __name__ == "__main__":
     cmd = sys.argv[1:] and sys.argv[1].lower()
 
@@ -24,7 +23,9 @@ if __name__ == "__main__":
 '''
 
 
-def INCLUDE(output, filename, start_tag="import", end_tag="if __name__ == '__main__'"):
+def include(output, filename, start_tag="import", end_tag="if __name__ == '__main__'"):
+    """include python file
+    """
     with open(filename, 'r') as gateway:
         print 'include:', filename
         if start_tag:
@@ -39,16 +40,46 @@ def INCLUDE(output, filename, start_tag="import", end_tag="if __name__ == '__mai
             else:
                 output.write(line)
 
-if __name__ == '__main__':
-    output_file = len(sys.argv) > 1 and sys.argv[1] or 'nkuwlan.py'
+
+def build(output_file):
+    """
+    build to
+    """
     print "building to: [%s] ..." % output_file
     output = open(output_file, 'w')
-    INCLUDE(output, "nkuwlan/__init__.py", start_tag=None, end_tag="__name__")
+    include(output, "nkuwlan/__init__.py", start_tag=None, end_tag="__name__")
     output.write('# THIS FILE AUTO BUILD AT--- %s ---\n' % (time.ctime()))
-    INCLUDE(output, "nkuwlan/gateway.py")
-    INCLUDE(output, "nkuwlan/config.py")
-    INCLUDE(output, "login.py", '# START_TAG #')
-    INCLUDE(output, "logout.py", '# START_TAG #')
-    output.write(main)
+    include(output, "nkuwlan/gateway.py", 'try:')
+    include(output, "nkuwlan/config.py")
+    include(output, "login.py", '# START_TAG #')
+    include(output, "logout.py", '# START_TAG #')
+    output.write(MAIN)
     output.close()
     print "Done!"
+
+
+def tabs(source, output):
+    """space to tabs"""
+    with open(source) as input_file:
+        lines = input_file.readlines()
+    with open(output, 'w') as output_file:
+        del lines[1]
+        for line in lines:
+            if not line:
+                break
+            for i in xrange(len(line)):
+                if line[i] == " ":
+                    output_file.write("\t")
+                else:
+                    output_file.write(line[i:])
+                    break
+
+        # output_file.writelines(lines)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        build('nkuwlan.py')
+    elif len(sys.argv) == 2:
+        build(sys.argv[1])
+    else:
+        tabs(sys.argv[1], sys.argv[2])
--- nkuwlan/__init__.py
+++ nkuwlan/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 __author__ = 'New Future'
 
 __name__ = "nkuwlan"
